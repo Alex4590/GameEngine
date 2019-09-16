ad3c75786fd8f25e43448f3d93a2334d38ffdb21
Alex <Alex45v90@mail.ru>
2019-09-17 00:32:00 +0300

    Init

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+
+
+public class Main {
+
+    public static void main(String[] args) throws LWJGLException {
+
+        Display.create();
+
+        while (!Display.isCloseRequested()) {
+            Display.update();
+        }
+
+        Display.destroy();
+
+    }
+
+}
