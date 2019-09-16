286a5201628cb7dde0273fe9ac80992444780989
Alex <Alex45v90@mail.ru>
2019-09-17 02:40:02 +0300

    Add DisplayManager to renderEngine

---
+++ src/main/java/renderEngine/DisplayManager.java
@@ -0,0 +1,43 @@
+package renderEngine;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.*;
+import org.lwjgl.opengl.DisplayMode;
+
+import java.awt.*;
+
+public class DisplayManager {
+
+    private static final int WIDTH = 1280;
+    private static final int HEIGHT = 720;
+    private static final int FPS_CAP = 120;
+
+
+    public static void CreateDisplay() {
+
+        ContextAttribs attribs = new ContextAttribs(3,2);
+        attribs.withForwardCompatible(true);
+        attribs.withProfileCore(true);
+
+        try {
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.create(new PixelFormat(), attribs);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        GL11.glViewport(0, 0, WIDTH, HEIGHT);
+
+    }
+
+    public static void UpdateDispaly() {
+
+        Display.sync(FPS_CAP);
+        Display.update();
+    }
+
+    public static void CloseDisplay() {
+        
+        Display.destroy();
+    }
+}
