f44306ee39a051287ee6b09458b0759c73397234
aleksandr.polyakov <alexworkaccent@gmail.com>
2019-11-14 15:19:28 +0300

    Edit shaders

--- src/main/shaders/vertexShader.glsl
+++ src/main/shaders/vertexShader.glsl
@@ -10,8 +10,9 @@ uniform mat4 transformationMatrix;
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 
-void main(void) {
-    gl_Position = projectionMatrix * viewMatrix * transformationMatrix * vec4(position, 1.0);
+void main(void){
+
+    gl_Position = projectionMatrix * viewMatrix * transformationMatrix * vec4(position,1.0);
     pass_textureCoordinates = textureCoordinates;
-    colour = vec3(position.x + 0.5, 0.0, position.y + 0.5);
+    colour = vec3(position.x + 0.5,0.0, position.y + 0.5);
 }
