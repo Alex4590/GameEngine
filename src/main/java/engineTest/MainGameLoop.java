9576eaf747783f011c1bc32a46f741d97910d87b
aleksandr.polyakov <alexworkaccent@gmail.com>
2019-11-15 19:09:51 +0300

    Add  Loading 3D Models | #9

--- src/main/java/engineTest/MainGameLoop.java
+++ src/main/java/engineTest/MainGameLoop.java
@@ -4,10 +4,7 @@ import entities.Camera;
 import entities.Entity;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
-import renderEngine.DisplayManager;
-import renderEngine.Loader;
-import renderEngine.RawModel;
-import renderEngine.Renderer;
+import renderEngine.*;
 import shadersClass.StaticShader;
 import texturesClass.ModelTexture;
 import texturesClass.TexturedModel;
@@ -22,88 +19,14 @@ public class MainGameLoop {
         StaticShader shader = new StaticShader();
         Renderer renderer = new Renderer(shader);
 
-        float[] vertices = {
-                -0.5f,0.5f,0,
-                -0.5f,-0.5f,0,
-                0.5f,-0.5f,0,
-                0.5f,0.5f,0,
 
-                -0.5f,0.5f,1,
-                -0.5f,-0.5f,1,
-                0.5f,-0.5f,1,
-                0.5f,0.5f,1,
-
-                0.5f,0.5f,0,
-                0.5f,-0.5f,0,
-                0.5f,-0.5f,1,
-                0.5f,0.5f,1,
-
-                -0.5f,0.5f,0,
-                -0.5f,-0.5f,0,
-                -0.5f,-0.5f,1,
-                -0.5f,0.5f,1,
-
-                -0.5f,0.5f,1,
-                -0.5f,0.5f,0,
-                0.5f,0.5f,0,
-                0.5f,0.5f,1,
-
-                -0.5f,-0.5f,1,
-                -0.5f,-0.5f,0,
-                0.5f,-0.5f,0,
-                0.5f,-0.5f,1
-        };
-
-        float[] textureCoords = {
-
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0
-        };
-
-        int[] indices = {
-                0,1,3,
-                3,1,2,
-                4,5,7,
-                7,5,6,
-                8,9,11,
-                11,9,10,
-                12,13,15,
-                15,13,14,
-                16,17,19,
-                19,17,18,
-                20,21,23,
-                23,21,22
-        };
-
-        RawModel model = loader.loadToVao(vertices,textureCoords,indices);
-        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("image-3")));
-        Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+        RawModel model = OBJLoader.loadObjModel("stall", loader);
+        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("stallTexture")));
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,-40),0,0,0,1);
         Camera camera = new Camera();
 
         while(!Display.isCloseRequested()) {
-            entity.increaseRotation(1, 1, 0);
+            entity.increaseRotation(0, 0.5f, 0);
             camera.move();
             renderer.prepare();
             shader.start();
